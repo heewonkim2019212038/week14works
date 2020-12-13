ad008875963d408d3db0278f35bb434ad8553b11
heewonkim2019212038 <71583971+heewonkim2019212038@users.noreply.github.com>
2020-12-14 05:03:25 +0900

    upload
    upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    console.log("Jquery Load Complete!")
+})
+
+$("#my_image1").attr("src","img/img2.jpg");
+console.log("메세지");
+$("#my_image2").attr("src","img/img3.jpg");
